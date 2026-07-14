b5bc239df20baf0fb2c1e6d2173c3281ba0c2450
Tou Fong Lee <leetoufong@gmail.com>
2026-07-13 23:33:04 -0400

    Format date to be more human friendly

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -5,6 +5,7 @@ const TaskList = (props) => {
         <section className="p-10">
             <ul className="[&>:not(:first-child)]:mt-3">
                 {tasks?.map((task) => {
+                    const date = task.date.split('T')[0].split('-').reverse().join('/');
                     const hours = Math.floor(task.time / (1000 * 60 * 60));
                     const minutes = Math.floor((task.time / (1000 * 60)) % 60);
                     const seconds = Math.floor((task.time / 1000) % 60);
@@ -12,7 +13,7 @@ const TaskList = (props) => {
                     return (
                         <li className="border p-5 rounded" key={task.id}>
                             <p>{task.title}</p>
-                            <p>{task.date}</p>
+                            <p>{date}</p>
                             <p>{hours != 0 && `${hours}h `} {minutes != 0 && `${minutes}m `} {seconds != 0 && `${seconds}s`}</p>
                             <button>Edit</button>
                         </li>
