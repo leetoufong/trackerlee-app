38b6aa15cd0fddd0ae831f71844215c8951aa53e
Tou Fong Lee <leetoufong@gmail.com>
2026-07-13 23:30:02 -0400

    Format time better on render

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,11 +3,18 @@ const TaskList = (props) => {
 
     return (
         <section className="p-10">
-            <ul>
+            <ul className="[&>:not(:first-child)]:mt-3">
                 {tasks?.map((task) => {
+                    const hours = Math.floor(task.time / (1000 * 60 * 60));
+                    const minutes = Math.floor((task.time / (1000 * 60)) % 60);
+                    const seconds = Math.floor((task.time / 1000) % 60);
+
                     return (
-                        <li key={task.id}>
-                            {task.title} / {task.date} / {task.time} <button>Edit</button>
+                        <li className="border p-5 rounded" key={task.id}>
+                            <p>{task.title}</p>
+                            <p>{task.date}</p>
+                            <p>{hours != 0 && `${hours}h `} {minutes != 0 && `${minutes}m `} {seconds != 0 && `${seconds}s`}</p>
+                            <button>Edit</button>
                         </li>
                     )
                 })}
